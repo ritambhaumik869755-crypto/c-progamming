a7414f0f95580b24e95fbd22770cd25d1c25a31f
Ritam Bhaumik  (ECE - 075) <ritambhaumik869755@gmail.com>
2026-02-08 20:54:40 +0530

    Fix prime number checking logic and cleanup code.c

--- Check if the number is prime or not.c
+++ Check if the number is prime or not.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
+int main(){
+    int n,i;
+    printf("enter a number: ");
+    scanf("%d",&n);
 
-int main() {
-    int main(){
-        int i,n;
-        printf("Enter a number: ");
-        scanf("d",&n);
-        
-        for(i = 2;i<n; i=i+1){
-            if(n % i==0){
-                printf("%d is not a prime number\n", n);
-                  
-            }
-            printf("%d is prime number", n);
-        }
+for(i=2;i<n;i=i+1){
+    if(n % i==0){
+        printf("%d is not a prime number\n", n);
+        return 0;
     }
-
-    return 0;
+    }
+    printf("%d is prime number", n);  
 }
